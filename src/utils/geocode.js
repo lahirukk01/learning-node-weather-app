3043f8a0abafd8f8333fda60d057644e55b3ea96
Lahiru Kariyawasam <lkksoftdev@gmail.com>
2020-10-17 00:48:55 +0530

    Configured only for github

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,11 +1,8 @@
 const axios = require('axios')
 
 const geocode = (location, callback) => {
-    // const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(location)}.json?` +
-    // `access_token=${process.env.MAPBOX_API_KEY}&limit=1`
-
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(location)}.json?` +
-        `access_token=pk.eyJ1IjoibGtrc29mdGRldiIsImEiOiJjazZ1YXk1OTYwMDBlM2VsZmd2bDRiYmt3In0.kTxk4l2ztsch3j9zT7w_TA&limit=1`
+    `access_token=${process.env.MAPBOX_API_KEY}&limit=1`
 
     axios.get(url)
         .then(response => {
